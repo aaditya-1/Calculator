1b9a52c6232ae03d65b2969848ed756a127cc392
aaditya-1 <71373951+aaditya-1@users.noreply.github.com>
2020-10-03 11:07:51 -0700

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,62 @@
+// function getHistory(){
+//     return document.getElementById("history-value").innerText;
+
+// }
+// // alert(getHistory())
+
+// function printHistory(num){
+//     document.getElementById("history-value").innerText = num;
+// }
+
+// function getOutput(){
+    
+//     document.getElementById("result-value").innerText;
+// }
+
+// function printOutput(num){
+//     if(num==""){
+//         return document.getElementById("result-value").innerText;
+//     }else{
+//     document.getElementById("result-value").innerText = (num);
+//     }
+// }
+
+// var operation = document.getElementsByClassName("operator");
+
+// for(var i = 0;i < operation.length;i++ ){
+
+//     operation[i].addEventListener('click',function(){
+//         alert("operation clicked: "+ this.id);
+//     }
+//     )
+// }
+// var number = document.getElementsByClassName("number")
+// for(var i = 0;i < number.length;i++ ){
+
+//     number[i].addEventListener('click',function(){
+//         // alert("number clicked: "+ this.id);
+//         var output = getOutput();
+//         if(output == NaN){
+//             output = output+this.id;
+//             printOutput(output);
+//         }
+//     }
+//     )
+// }
+
+
+
+// // function roundOff(num){
+// //     var n = Number(num);
+// //     var value = n.toLocaleString("en");
+// //     return value;
+// // }
+
+// // function reverse(num){
+// //     return Number(num.replace(/,/g,""));
+// // }
+
+
+// // alert(reverse(getOutput()));
+// // console.log(reverse());
+
